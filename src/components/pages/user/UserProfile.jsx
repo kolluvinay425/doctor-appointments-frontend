021fc028e631aa629d565267fbcd84fc4bb20c88
kolluvinay425 <kolluvinay425@gmail.com>
2022-01-30 00:14:18 +0100

    update

--- src/components/pages/user/UserProfile.jsx
+++ src/components/pages/user/UserProfile.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "../../../styles/userProfile.css";
-
+import { BE_URL } from "../../../helpers/apiFetches";
+import API from "../../../helpers/apiFetches";
+// import axios from "axios";
 function UserProfile() {
   const userProfile = useSelector((user) => user.user);
+  const [fileName, setFileName] = useState(null);
+  const updateImage = async (e) => {
+    //setFileName(e.target.files[0]);
+    //console.log("image", fileName);
+
+    //const image = formData.get("image");
+    // console.log("image55", form);
+
+    try {
+      const form = new FormData();
+      form.append("image", fileName);
+      console.log("image55", form);
+      const { data } = await API.post(`user/update-profile-image`, {
+        form,
+      });
+
+      if (data) {
+        console.log("Image Updated", data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div style={{ height: "100vh" }}>
       <br />
@@ -20,9 +45,16 @@ function UserProfile() {
         <i className="bi bi-pencil-square ">edit</i>
       </h4> */}
       <div class="fileUpload">
-        <input type="file" class="upload" />
+        <input
+          onChange={(e) => {
+            setFileName(e.target.files[0]);
+          }}
+          type="file"
+          class="upload"
+        />
         <span>
           <i className="bi bi-pencil-square ">edit</i>
+          <button onClick={updateImage}>save</button>
         </span>
       </div>
       <br />
